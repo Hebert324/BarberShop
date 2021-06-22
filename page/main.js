e041ad48ee35ff13de03df84e8d365ce8cbb2b0c
Hebert324 <hebertrocha76@gmail.com>
2021-06-22 16:35:36 -0300

    ending the menu in javascript

--- page/main.js
+++ page/main.js
@@ -1,6 +1,19 @@
+//abre e fecha o menu quando clicar no icone.
 const nav = document.querySelector('#header nav')
-const toggle = document.querySelector('nav .toggle')
+const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-    console.log(element)
+    element.addEventListener('click',() => {
+        //Ele esta pegando o nav e vendo a lista de classes dele e adicionando ou tirando a classe show nele.
+        nav.classList.toggle('show')
+    })
+}
+
+//Quando clicar em um item do menu, esconder o menu.
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+    link.addEventListener('click',() => {
+        nav.classList.remove('show')
+    })
 }
